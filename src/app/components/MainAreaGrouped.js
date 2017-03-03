65cf16c13dbd7d11329416697c6b71f77f5fe9f5
Wojciech Telus <wojciech.telus@contour-technology.com>
2017-03-03 23:26:37 +0100

    Adding mapping of MainAreaItems problem

--- src/app/components/MainAreaGrouped.js
+++ src/app/components/MainAreaGrouped.js
@@ -4,13 +4,49 @@ import {Table, TableBody } from 'material-ui/Table';
 
 import MainAreaItems from './MainAreaItems'
 
+import createTableRow from './MainAreaItems'
+
+var  rows = [
+    {
+        avatarStyle : {
+            width: "10px"
+        },
+        Image,
+        itemNameStyle: {
+            width: "150px"
+        },
+        itemName: "First Item Name"
+    },
+    {
+        avatarStyle : {
+            width: "10px"
+        },
+        Image,
+        itemNameStyle: {
+            width: "150px"
+        },
+        itemName: "First Item Name"
+    },
+    {
+        avatarStyle : {
+            width: "10px"
+        },
+        Image,
+        itemNameStyle: {
+            width: "150px"
+        },
+        itemName: "First Item Name"
+    }
+];
+
 class MainAreaGrouped extends Component {
 
     render(){
+        const MainAreaItems = rows.map(createTableRow);
         return (
             <Table>
                 <TableBody displayRowCheckbox={ false }>
-                    <MainAreaItems/>
+                    { MainAreaItems }
                 </TableBody>
             </Table>
         );
--- src/app/components/MainAreaItems.js
+++ src/app/components/MainAreaItems.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table';
+import { TableRow, TableRowColumn } from 'material-ui/Table';
 
 import Avatar from 'material-ui/Avatar'
 import Image from '../../../public/uxceo-128.jpg'
@@ -10,105 +10,54 @@ import {blue300, indigo900} from 'material-ui/styles/colors';
 
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 
+const createTableRow = function(row, i){
+    const {avatarStyle, itemNameStyle, itemName} = row;
+    return <TableRow hoverable={true} striped={true} key={i}>
 
-var  rows = [
-    {
-        avatarStyle : {
-            width: "10px"
-        },
-        Image,
-        itemNameStyle: {
-            width: "150px"
-        },
-        itemName: "First Item Name"
-    },
-    {
-        avatarStyle : {
-            width: "10px"
-        },
-        Image,
-        itemNameStyle: {
-            width: "150px"
-        },
-        itemName: "First Item Name"
-    },
-    {
-        avatarStyle : {
-            width: "10px"
-        },
-        Image,
-        itemNameStyle: {
-            width: "150px"
-        },
-        itemName: "First Item Name"
-    }
-];
+        <TableRowColumn style={ avatarStyle }>
+            <Avatar
+                src={Image}
+                size={30}
+            />
+        </TableRowColumn>
 
-class MainAreaItems extends Component {
+        <TableRowColumn style={ itemNameStyle }>
+            <b> { itemName } </b>
+        </TableRowColumn>
 
-    renderTableRows(tableRow){
-        const {avatarStyle, Image, itemNameStyle, itemName} = tableRow;
-        return (
-            <TableRow hoverable={true} striped={true}>
+        <TableRowColumn> User </TableRowColumn>
 
-                <TableRowColumn style={ avatarStyle }>
-                    <Avatar
-                        src={Image}
-                        size={30}
-                    />
-                </TableRowColumn>
+        <TableRowColumn>
+            <Chip
+                backgroundColor={blue300}
+                labelColor={"white"}>
+                <Avatar size={32} color={blue300} backgroundColor={indigo900} style={{ "color": "white"}}>
+                    I
+                </Avatar>
+                Colored Chip
+            </Chip>
+        </TableRowColumn>
 
-                <TableRowColumn style={ itemNameStyle }>
-                    <b> { itemName } </b>
-                </TableRowColumn>
+        <TableRowColumn>
+            Tags
+        </TableRowColumn>
+        <TableRowColumn>
+            Tags
+        </TableRowColumn>
+        <TableRowColumn>
+            Tags
+        </TableRowColumn>
+        <TableRowColumn>
+            Tags
+        </TableRowColumn>
+        <TableRowColumn>
+            Tags
+        </TableRowColumn>
+        <TableRowColumn style={{"float": "right"}}>
+            <FloatingActionButton mini={true} disabled={true}>
+            </FloatingActionButton>
+        </TableRowColumn>
+    </TableRow>
+};
 
-                <TableRowColumn> User </TableRowColumn>
-
-                <TableRowColumn>
-                    <Chip
-                        backgroundColor={blue300}
-                        labelColor={"white"}>
-                        <Avatar size={32} color={blue300} backgroundColor={indigo900} style={{ "color": "white"}}>
-                            I
-                        </Avatar>
-                        Colored Chip
-                    </Chip>
-                </TableRowColumn>
-
-                <TableRowColumn>
-                    Tags
-                </TableRowColumn>
-                <TableRowColumn>
-                    Tags
-                </TableRowColumn>
-                <TableRowColumn>
-                    Tags
-                </TableRowColumn>
-                <TableRowColumn>
-                    Tags
-                </TableRowColumn>
-                <TableRowColumn>
-                    Tags
-                </TableRowColumn>
-                <TableRowColumn style={{"float": "right"}}>
-                    <FloatingActionButton mini={true} disabled={true}>
-                    </FloatingActionButton>
-                </TableRowColumn>
-            </TableRow>
-        );
-    }
-
-    render(){
-        return (
-            <TableRow hoverable={true} striped={true}>
-
-            </TableRow>
-        );
-
-    }
-
-}
-
-
-
-export default MainAreaItems;
+export default createTableRow;
