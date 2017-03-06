ff304754967d9957886d9d62841139ecc22004a5
Dmitrij Rysanow <dmitrij.rysanow@gmail.com>
2017-03-06 11:30:23 +0100

    after review fixes

--- src/app/components/RightNavBar.js
+++ src/app/components/RightNavBar.js
@@ -5,13 +5,12 @@
 import React, { Component } from 'react';
 import {Card, CardMedia} from "material-ui/Card";
 import AppBar from "material-ui/AppBar";
-import IconButton from "material-ui/IconButton";
-import NavigationClose from "material-ui/svg-icons/navigation/close";
+import MoreVert from "material-ui/MoreVert";
 import Image from "../../../public/nature-600-337.jpg";
 import {red500} from "material-ui/styles/colors";
 import ItemDetails from "./ItemDetails";
 
-const appBarContextMenu = <IconButton><NavigationClose /></IconButton>;
+const appBarContextMenu = <IconButton><MoreVert /></IconButton>;
 class RightNavBar extends Component {
     constructor(props) {
         super(props);
